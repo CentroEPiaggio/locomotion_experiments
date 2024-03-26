3728036c22957e83115eecc6e42bf8492598b58b
joshphoo <josh.pho@gmail.com>
2024-03-26 17:25:09 +0100

    real robot launch files

--- locomotion_experiments/getup.py
+++ locomotion_experiments/getup.py
@@ -13,40 +13,74 @@ class GetUp(Node):
     def __init__(self):
         super().__init__('getup_controller')
 
-        # Topic names
-        self.declare_parameter('joint_target_pos_topic', '/joint_controller/command')
-        self.joint_target_pos_topic     = self.get_parameter('joint_target_pos_topic').get_parameter_value().string_value
+        with open(r'src/filc_controller/config/MULINEX_traj_opt_abs_theta=13_SEA_no_dampers_obj_CoT.yaml') as file:
+            data = yaml.safe_load(file)     
+
+        x_ref = np.array(data['x'])
+        z_ref = np.array(data['z'])
+        alpha_ref = -np.array(data['alpha'])
+        x_dot_ref = np.array(data['x_dot'])
+        z_dot_ref = np.array(data['z_dot'])
+        alpha_dot_ref = -np.array(data['alpha_dot'])
+        theta_2 = np.array(data['theta_2'])[:-1]
+        theta_3 = np.array(data['theta_3'])[:-1]
+        theta_4 = np.array(data['theta_4'])[:-1]
+        theta_5 = np.array(data['theta_5'])[:-1]
+        theta_2_dot = np.array(data['theta_2_dot'])[:-1]
+        theta_3_dot = np.array(data['theta_3_dot'])[:-1]
+        theta_4_dot = np.array(data['theta_4_dot'])[:-1]
+        theta_5_dot = np.array(data['theta_5_dot'])[:-1]
+
+        self.default_dof    = (
+        theta_4[0]*-1, 
+        (np.pi + theta_3[0])*-1, 
+        theta_4[0], 
+        (np.pi + theta_3[0]),
+        -theta_5[0],
+        -theta_2[0],
+        theta_5[0],
+        theta_2[0]
+        )
+
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('joint_state_topic', rclpy.Parameter.Type.STRING),
+                ('joint_target_torque_topic', rclpy.Parameter.Type.STRING),
+                ('cmd_vel_topic', rclpy.Parameter.Type.STRING),
+                ('imu_topic', rclpy.Parameter.Type.STRING),
+                ('rate', rclpy.Parameter.Type.INTEGER),
+                ('use_imu', rclpy.Parameter.Type.BOOL),
+                ('imu_topic', rclpy.Parameter.Type.STRING),
+                ('simulation', rclpy.Parameter.Type.BOOL),  
+                ('traj_gen_joint_reference_topic', rclpy.Parameter.Type.STRING),
+                ('njoint', rclpy.Parameter.Type.INTEGER), 
+                ('default_hip_angle', rclpy.Parameter.Type.INTEGER), 
+                ('default_knee_angle', rclpy.Parameter.Type.INTEGER),
+                ('joint_names', rclpy.Parameter.Type.STRING_ARRAY),  
+                ('T_idx', rclpy.Parameter.Type.INTEGER_ARRAY),  
+                ('n_iter', rclpy.Parameter.Type.INTEGER),  
+                ('variance', rclpy.Parameter.Type.DOUBLE), 
+                ('Q', rclpy.Parameter.Type.DOUBLE), 
+                ('S', rclpy.Parameter.Type.DOUBLE),  
+                ('scaling', rclpy.Parameter.Type.DOUBLE_ARRAY),              
+                ('k_f', rclpy.Parameter.Type.DOUBLE), 
+                ('initial_hip_angle', rclpy.Parameter.Type.INTEGER), 
+                ('initial_knee_angle', rclpy.Parameter.Type.INTEGER),                 
+            ]
+        )
+
+        self.joint_target_torque_topic = self.get_parameter('joint_target_torque_topic').get_parameter_value().string_value
+        self.rate = self.get_parameter('rate').get_parameter_value().integer_value
+        self.njoint = self.get_parameter('njoint').get_parameter_value().integer_value
+        self.joint_names = self.get_parameter('joint_names').get_parameter_value().string_array_value        
 
 
         self.deltaT = 5.0
-        self.rate = 100
-        self.default_dof = np.array([
-             2.094,     # flip
-             -2.094,    # flip
-             -2.094,
-             2.094,
-             -0.7854,   # flip
-             0.7854,    # flip
-             0.7854,
-             -0.7854,
-        ]) 
-
-        # Initialize joint publisher/subscriber
-        self.njoint = 8
-
-        self.joint_names=(
-            'LF_HFE',   # flip
-            'LH_HFE',   # flip
-            'RF_HFE',
-            'RH_HFE',
-            'LF_KFE',   # flip
-            'LH_KFE',   # flip
-            'RF_KFE',
-            'RH_KFE',
-        )
-
-        self.joint_target_pos_pub = self.create_publisher(JointsCommand, self.joint_target_pos_topic, 10)
-        self. i = 0
+        
+        self.joint_target_pos_pub = self.create_publisher(JointsCommand, self.joint_target_torque_topic, 10)
+        self.i = 0
         self.joint_pos = np.zeros((self.njoint, 1))
         self.joint_vel = {self.joint_names[i]:0.0 for i in range(self.njoint)}
    
