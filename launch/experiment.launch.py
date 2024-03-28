5cde98af5ab62554973cf7149b133ae7b56d75e2
joshphoo <josh.pho@gmail.com>
2024-03-28 11:08:33 +0100

    Revert "real robot launch files"
    This reverts commit 3728036c22957e83115eecc6e42bf8492598b58b.

--- launch/experiment.launch.py
+++ launch/experiment.launch.py
@@ -15,16 +15,14 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
 
-
-
-        # cmdvel_node=Node(
-        #         package = 'locomotion_experiments',
-        #         name = 'cmd_vel_node',
-        #         executable = 'cmd_vel_node',
-        #         parameters = [{'publication_rate': 200},
-        #                 {'duration': 5.0},
-        #                 {'start_delay': 1.0}]
-        # )
+        cmdvel_node=Node(
+                package = 'locomotion_experiments',
+                name = 'cmd_vel_node',
+                executable = 'cmd_vel_node',
+                parameters = [{'publication_rate': 200},
+                        {'duration': 5.0},
+                        {'start_delay': 1.0}]
+        )
 
         # launch argument: movie name
         # movie_name = LaunchConfiguration('v', default='test')
@@ -44,11 +42,11 @@ def generate_launch_description():
         # time_stamp = time.strftime("%Y_%m_%d_%H-%M-%S")
         #bag_filename = 'exp_' + index + '_mv_'+ movie_name+ '_' + time_stamp + '.bag'
 
-        # policy = IncludeLaunchDescription(
-        #         PythonLaunchDescriptionSource(
-        #                 [PathJoinSubstitution([FindPackageShare("rlg_quad_controller"), "launch", "mulinex_simulation.launch.py"])]
-        #         ),
-        # )
+        policy = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                        [PathJoinSubstitution([FindPackageShare("rlg_quad_controller"), "launch", "mulinex_simulation.launch.py"])]
+                ),
+        )
 
         # TODO: stops bag recording and policy node when cmd_vel_node is done
 
@@ -57,7 +55,7 @@ def generate_launch_description():
                 cmd=['ros2', 'bag', 'record', '-a'],
                 output='screen'
                 ),
-                # cmdvel_node,
-                # policy,
+                cmdvel_node,
+                policy,
                 
         ])
