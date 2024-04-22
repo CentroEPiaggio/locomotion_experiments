f5206b7e476961b9bc745a212e5f65ab1b21c598
Cionix0 <cionix90@gmail.com>
2024-04-22 10:48:25 +0200

    add ramp to cmd_vel node

--- locomotion_experiments/cmd_vel_node.py
+++ locomotion_experiments/cmd_vel_node.py
@@ -15,8 +15,8 @@ class CmdVelNode(Node):
         super().__init__('cmd_vel_node')
         #### parameters
         self.declare_parameter('publication_rate', 200) # Hz rate at which to publish cmd_vel
-        self.declare_parameter('duration', 5.0)         # s net duration of the experiment
-        self.declare_parameter('start_delay', 20.0)      # s delay before starting tp send ref
+        self.declare_parameter('duration', 3.0)         # s net duration of the experiment
+        self.declare_parameter('start_delay', 5.0)      # s delay before starting tp send ref
         self.declare_parameter('shutdown_delay', 2.0)   # s delay before shutting down the node
 
 
@@ -44,19 +44,20 @@ class CmdVelNode(Node):
         msg.linear.x = vx
         msg.angular.z = w  
         self.publisher_.publish(msg)
-        # self.get_logger().info('Publishing: "%s"' % msg.linear.x)
+        self.get_logger().info('Publishing: "%s"' % msg.linear.x)
 
 
     def vel_function(self, t):
         """ Function that returns the desired velocity vector as a function of time.
         """
-        vx = 1.0
+        vx = 0.8
         w = 0.0
         # return non zero only if t is in the interval [start_delay, start_delay + duration]
         if t > self.start_delay and t < (self.start_delay + self.duration):
             te = t - self.start_delay
-            # example: ramp
-            # vx = 0.3 * te / self.duration
+            # trapezoidal signal. 10% rise time
+            vxt = min(vx * te/(self.duration/10.0), vx, vx - vx*(te - 0.9*self.duration)*10.0/(self.duration))
+            vxt = vx * te/self.duration
             # example: sinusoidal
             # vx = 0.3 * math.sin(2*math.pi*te/self.duration)
             return [vx, w]
@@ -67,7 +68,7 @@ class CmdVelNode(Node):
             return [0.0, 0.0]
         else:
             return [0.0, 0.0] 
-
+# /home/jacopo/Documents/locosim_ws/src/locomotion_experiments/locomotion_experiments/cmd_vel_node.py
 
 def main(args=None):
     rclpy.init(args=args)
